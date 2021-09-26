71da29bc9300d285dd73a7387569f40f6f9d96a3
Vadym Borodin <borodean@gmail.com>
2021-09-26 21:17:28 +0300

    Add Cyrillic Bilingual (#3)

---
+++ Casks/cyrillic-bilingual.rb
@@ -0,0 +1,11 @@
+cask "cyrillic-bilingual" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/muromec/bandera-layout/archive/refs/heads/master.zip"
+  name "Cyrillic Bilingual"
+  homepage "https://github.com/muromec/bandera-layout"
+
+  artifact "Cyrillic bilingual.bundle",
+           target: "#{ENV["HOME"]}/Library/Keyboard Layouts/Cyrillic bilingual.bundle"
+end
