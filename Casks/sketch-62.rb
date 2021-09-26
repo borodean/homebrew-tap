b2b932a7490b71a465b27bfa3fc7f828d3669d7e
Vadym Borodin <borodean@gmail.com>
2021-09-26 20:00:48 +0300

    Add Sketch v62 (#1)

---
+++ Casks/sketch-62.rb
@@ -0,0 +1,25 @@
+cask "sketch-62" do
+  version "62-91390"
+  sha256 "ebdad1b3751f42818cada98c160c1a67177d5790120bb077d03c8786a68a534d"
+
+  url "https://download.sketchapp.com/sketch-#{version}.zip"
+  appcast "https://download.sketchapp.com/sketch-versions.xml"
+  name "Sketch"
+  homepage "https://www.sketchapp.com/"
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Sketch.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*",
+    "~/Library/Application Support/com.bohemiancoding.sketch3",
+    "~/Library/Caches/com.bohemiancoding.sketch3",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3",
+    "~/Library/Logs/com.bohemiancoding.sketch3",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.plist",
+    "~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies",
+  ]
+end
