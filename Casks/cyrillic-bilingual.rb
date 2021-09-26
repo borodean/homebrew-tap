4b6be6e63fbeba35f6f69f36598cd7bf83e420f7
Vadym Borodin <borodean@gmail.com>
2021-09-26 21:14:57 +0300

    Disable the SHA256 check

--- Casks/cyrillic-bilingual.rb
+++ Casks/cyrillic-bilingual.rb
@@ -1,8 +1,8 @@
 cask "cyrillic-bilingual" do
-  version "1.2"
-  sha256 "d096250b35a9c56c036b0fcc45ca824dffebf7c75408f9859e648d2fff45a7d5"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/muromec/bandera-layout/archive/1a4b4cb53586144576fdc764ef62695bb7abaf0e.zip"
+  url "https://github.com/muromec/bandera-layout/archive/refs/heads/master.zip"
   name "Cyrillic Bilingual"
   homepage "https://github.com/muromec/bandera-layout"
 
